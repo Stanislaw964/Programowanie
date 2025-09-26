9b6742dca6559393c52a92105a8c93c542fe20ca
Sala213 <Sala213@ezse.pl>
2025-09-26 13:15:43 +0200

    koniec lekcj

--- Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
+++ Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -1,20 +1,75 @@
-// FirstConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+//Napisz program który wczyta liczbê od u¿ytkownika i j¹ wyœwietli na konsoli
+void task1()
+{
+    //wczytanie liczby
+        //deklaracja zmiennych          1
+        //pobranie liczby
+            //informacja co chcemy      2
+            //oczekiwanie na liczbê     3
+    //wyœwietlenie na konsoli           4
+
+    //1
+    int numberFromUser; //number_from_user
+    //2
+    std::cout << "Podaj liczbê ca³kowit¹:\n";
+    //3
+    std::cin >> numberFromUser;
+    //4
+    std::cout << "Poda³eœ " << numberFromUser << "\n";
+}
+
+//Program obliczaj¹cy œredni¹ arytmetyczn¹ dwóch liczb.
+void task2()
+{
+    int firstNumber, secondNumber;
+
+    std::cout << "Podaj pierwsz¹ liczbê:\n";
+    std::cin >> firstNumber;
+
+    std::cout << "Podaj drug¹ liczbê:\n";
+    std::cin >> secondNumber;
+
+    int avg;
+
+    avg = (firstNumber + secondNumber) / 2;
+
+    std::cout << "Wynik œredniej: " << avg << "\n";
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    task2();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*
+
+Zmienna - pewien obszar w pamiêci operacyjnej, w której mo¿na
+w danej chwili przechowaæ tylko jedn¹ dan¹.
+
+Instrukcja daklaracji zmiennej:
+typ_zmienej nazwaZmiennej;
+
+Typ zmiennej - wielkoœæ obszaru pamiêci, interpretacja ci¹gu bitów
+
+int - 4 bajtowa liczba ca³kowita ze znakiem <-2 147 483 648, 2 147 483 647>
+
+Nazwa zmiennej - nazwa obszaru w pamiêci, identyfikator
+Warunki niezbêdne:
+* dozwolone znaki:
+    - alfabet angielski aA-zZ
+    - cyfry arabskie 0-9
+    - podkreœlenie (pod³oga) _
+* pierwszym znakiem nie mo¿e byæ cyfra
+* unikalny w swoim zakresie widocznoœci
+* nie mo¿e to byæ s³owo kluczowe (zarezerwowane) danego jêzyka
+
+Warunki programistów:
+* nazwa zmiennej powinna oddawaæ charakter przechowywanych danych
+* jeœli wiele s³ów to w miejscu spacji podkreœlenie lub zaczynaj¹c od drugiego
+  s³owa piszemy je z du¿ej litery
+* piszemy po angielsku
+*/
