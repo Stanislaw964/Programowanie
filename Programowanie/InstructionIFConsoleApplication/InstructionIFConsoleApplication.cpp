a46ea959dd8a547b4ab0daceb73357b9704bd0fd
Sala213 <Sala213@ezse.pl>
2025-10-17 13:18:47 +0200

    nara

--- Programowanie/InstructionIFConsoleApplication/InstructionIFConsoleApplication.cpp
+++ Programowanie/InstructionIFConsoleApplication/InstructionIFConsoleApplication.cpp
@@ -1,34 +1,70 @@
 // InstructionIFConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+//< - mniejszy
+//> - wiêkszy
+//<= - minejszy b¹æ równy
+//>= - wiêkrzy b¹æ równy
+//== - równy
+//!=  ró¿ny
+
+
 #include <iostream>
 
 void task1()
 {
-
-
 	int numberFromUser;
+
 	std::cout << "Podaj liczbê ca³kowit¹:\n";
 	std::cin >> numberFromUser;
 
 	if (numberFromUser > 0)
 		std::cout << "Poda³eœ liczbe dodatni¹\n";
 
+	if (numberFromUser < 0)
+		std::cout << "Poda³eœ liczbe ujemn¹\n";
+
+	if (numberFromUser == 0)
+		std::cout << "Poda³eœ liczbe 0\n";
+}
+
+void task2()
+{
+	int numer,reszta;
+
+	std::cout << "Podaj liczbê ca³kowit¹:\n";
+	std::cin >> numer;
+
+	reszta = numer % 2;
+
+	if (reszta == 0)
+		std::cout << "Liczba jest parzysta\n";
+	else
+		std::cout << "Liczba jest nie parzysta\n";
+}
+//Napisz program który stwierdzi czy liczba jest z zakresy 1 do 10
+void task3()
+{
+	int numer,liczba;
+
+	std::cout << "Podaj liczbê ca³kowit¹:\n";
+	std::cin >> numer;
+
+	if (numer >= 1)
+	{
+		if (numer <= 10)
+			std::cout << "liczba jest w zakresie 1 do 10\n";
+		else 
+			std::cout << "liczba nie jest w zakresie 1 do 10\n";
+	}
 }
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
-	task1();
+	task3();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
